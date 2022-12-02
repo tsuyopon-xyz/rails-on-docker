faf69595e4546151c5c29d44c7333fe17e449e14
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-02 17:36:09 +0900

    Done 7.1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    puts '=============='
+    p params
+    puts '=============='
+    @user = User.find(params[:id])
+  end
 end
