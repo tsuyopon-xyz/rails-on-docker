8b8c99cf951e1892ea776d9089c11c6ba543dbb2
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-08 17:15:24 +0900

    Done 10.4.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_07_132618) do
+ActiveRecord::Schema.define(version: 2022_12_08_075855) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_12_07_132618) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
