3b98b7bc95ece51118b5c03f0f5e06d4b589c085
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-09 14:10:40 +0900

    13.1.3

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,10 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    # @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+
+    # このコードは慣習的に正しい
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
