2c18054a191d9efcd22dc0bbe2e3286c7e0fb21a
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-09 19:19:31 +0900

    13.4.3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,4 +14,9 @@ class Micropost < ApplicationRecord
       less_than: 5.megabytes,
       message: "should be less than 5MB"
     }
+
+  # 表示用のリサイズ済み画像を返す
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
