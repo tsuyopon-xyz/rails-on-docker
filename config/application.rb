5894e7793de78ed57d1f7357c61aac2cd1f03bc6
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-10 13:56:10 +0900

    Done 14.2.5

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
