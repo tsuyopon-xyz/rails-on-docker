e1cfd292dd9cac65274d3d90c9a0f22a1f45a3b2
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-10 12:51:18 +0900

    Done 14.1.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
+
+  # source: :followerは省略可能. Railsが内部で、":followers" から単数系"follower"に変換して、外部キーfollower_idで探してくれるため
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
